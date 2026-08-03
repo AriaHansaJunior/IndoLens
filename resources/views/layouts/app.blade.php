623471279f7a5b7c0014de82aeb5df3788e93c20
AriaHansaJunior <167549815+AriaHansaJunior@users.noreply.github.com>
2026-08-03 13:26:05 +0700

    feat: update asset loading logic in app layout for better section handling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-    @yield('assets', \Illuminate\Support\Facades\Vite::use('build')->withEntryPoints(['resources/css/app.css', 'resources/js/app.js']))
+    @hasSection('assets')
+        @yield('assets')
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 </head>
 <body>
 
